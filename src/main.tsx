714fdd2964f84aa46af5011055f224d78466eaae
Maksim <vl4dimirov@gmail.com>
2023-12-07 09:04:25 +0300

    add: add eslint, folder structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from '@/app/App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 )
 
 // Remove Preload scripts loading
@@ -14,5 +13,5 @@ postMessage({ payload: 'removeLoading' }, '*')
 
 // Use contextBridge
 window.ipcRenderer.on('main-process-message', (_event, message) => {
-  console.log(message)
+    console.log(message)
 })
--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,26 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    electron({
-      main: {
-        // Shortcut of `build.lib.entry`.
-        entry: 'electron/main.ts',
-      },
-      preload: {
-        // Shortcut of `build.rollupOptions.input`.
-        // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
-        input: path.join(__dirname, 'electron/preload.ts'),
-      },
-      // Ployfill the Electron and Node.js built-in modules for Renderer process.
-      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
-      renderer: {},
-    }),
-  ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    plugins: [
+        react(),
+        electron({
+            main: {
+                // Shortcut of `build.lib.entry`.
+                entry: 'electron/main.ts',
+            },
+            preload: {
+                // Shortcut of `build.rollupOptions.input`.
+                // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
+                input: path.join(__dirname, 'electron/preload.ts'),
+            },
+            // Ployfill the Electron and Node.js built-in modules for Renderer process.
+            // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+            renderer: {},
+        }),
+    ],
 })
